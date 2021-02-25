790e9f0b523b5e2b9ea87fc4683fbeee9d762c27
Michael Bicakcioglu <michael.bicakcioglu@student.vinci.be>
2021-02-25 18:47:22 +0100

    feat - ajout du frontend + login

---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import { setLayout } from "./utils/render.js";
+import HomePage from "./Components/HomePage.js";
+import {Router} from "./Components/Router.js";
+import Navbar from "./Components/Navbar.js";
+/* use webpack style & css loader*/
+import "./stylesheets/style.css";
+/* load bootstrap css (web pack asset management) */
+import 'bootstrap/dist/css/bootstrap.css';
+/* load bootstrap module (JS) */
+import 'bootstrap';
+
+const HEADER_TITLE = "LiVi Satcho Antiquaire";
+const FOOTER_TEXT = "Contact : livisatcho@hotmail.com";
+
+Navbar();
+Router();
+
+setLayout(HEADER_TITLE, FOOTER_TEXT);
---
+++ frontend/src/utils/render.js
@@ -0,0 +1,14 @@
+
+
+/**
+ * setLayout allows to display specific information in an HTML template
+ * containing those paramters as id to text elements (h4, h5, title)
+ * @param {headerTitle} headerTitle
+ * @param {footerText} footerText
+ */
+function setLayout(headerTitle, footerText) {
+  document.querySelector("#headerTitle").innerText = headerTitle;
+  document.querySelector("#footerText").innerText = footerText;
+}
+// named export
+export {setLayout};
