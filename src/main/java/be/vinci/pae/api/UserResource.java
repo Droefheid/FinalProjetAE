2993fff15552113627168edc10c7df5fe719caaa
Dorian Fouvez <dorian.fouvez@student.vinci.be>
2021-03-04 13:35:29 +0100

    refactor for good skeleton

--- src/main/java/be/vinci/pae/api/UserResource.java
+++ src/main/java/be/vinci/pae/api/UserResource.java
@@ -1,10 +1,15 @@
 package be.vinci.pae.api;
 
+import be.vinci.pae.api.utils.Json;
 import be.vinci.pae.domaine.UserFactory;
 import be.vinci.pae.services.DataServiceUserCollection;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Singleton
 @Path("/users")
@@ -19,5 +24,11 @@ public class UserResource {
   // TODO UserDTO ????
 
   // TODO methode login
+  @POST
+  @Path("login")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response login(Json json) {
+    return null;
+  }
 
 }
