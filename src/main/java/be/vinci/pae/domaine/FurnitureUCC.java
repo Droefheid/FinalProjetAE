ae88e830aae4308f9437d66be0fce96cf8a52bb7
Cyprien Lekeu <cyprien.lekeu@student.vinci.be>
2021-03-25 16:13:24 +0100

    furnitures

---
+++ src/main/java/be/vinci/pae/domaine/FurnitureUCC.java
@@ -0,0 +1,13 @@
+package be.vinci.pae.domaine;
+
+public interface FurnitureUCC {
+
+	FurnitureDTO add(FurnitureDTO furniture);
+	
+	FurnitureDTO getById(int id);
+	
+	FurnitureDTO getAll();
+	
+	//TODO add more methods
+	
+}
---
+++ src/main/java/be/vinci/pae/domaine/FurnitureUCCImpl.java
@@ -0,0 +1,26 @@
+package be.vinci.pae.domaine;
+
+public class FurnitureUCCImpl implements FurnitureUCC {
+
+	//TODO inject furniture DAO
+	
+	@Override
+	public FurnitureDTO add(FurnitureDTO furniture) {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+	@Override
+	public FurnitureDTO getById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FurnitureDTO getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
