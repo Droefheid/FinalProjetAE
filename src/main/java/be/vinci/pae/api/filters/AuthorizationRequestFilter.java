8f9bfc8b547590ca4e520599dfbe20445cf5d61b
Dorian Fouvez <dorian.fouvez@student.vinci.be>
2021-04-01 19:26:16 +0200

    refactor jenkins

--- src/main/java/be/vinci/pae/api/filters/AuthorizationRequestFilter.java
+++ src/main/java/be/vinci/pae/api/filters/AuthorizationRequestFilter.java
@@ -1,55 +1,24 @@
 package be.vinci.pae.api.filters;
 
 import java.io.IOException;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.TokenExpiredException;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.jwt.interfaces.JWTVerifier;
-import be.vinci.pae.api.utils.PresentationException;
-import be.vinci.pae.domaine.UserUCC;
-import be.vinci.pae.utils.Config;
-import jakarta.inject.Inject;
+import be.vinci.pae.domaine.UserDTO;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.ext.Provider;
 
 
 @Singleton
 @Provider
 @Authorize
-public class AuthorizationRequestFilter implements ContainerRequestFilter {
-
-  private final Algorithm jwtAlgorithm = Algorithm.HMAC256(Config.getProperty("JWTSecret"));
-  private final JWTVerifier jwtVerifier =
-      JWT.require(this.jwtAlgorithm).withIssuer("auth0").build();
-
-  @Inject
-  private UserUCC userUCC;
+public class AuthorizationRequestFilter extends AuthorizeAbstract
+    implements ContainerRequestFilter {
 
   @Override
   public void filter(ContainerRequestContext requestContext) throws IOException {
-    String token = requestContext.getHeaderString("Authorization");
-    if (token == null) {
-      requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED)
-          .entity("A token is needed to access this resource").build());
-    } else {
-      if (token.startsWith("\"") && token.endsWith("\"")) {
-        token = token.substring(1, token.length() - 1);
-      }
-      DecodedJWT decodedToken = null;
-      try {
-        decodedToken = this.jwtVerifier.verify(token);
-      } catch (TokenExpiredException e) {
-        throw new PresentationException("Expired token", e, Status.UNAUTHORIZED);
-      } catch (Exception e) {
-        throw new PresentationException("Malformed token", e, Status.UNAUTHORIZED);
-      }
-      requestContext.setProperty("user",
-          this.userUCC.getUser(decodedToken.getClaim("user").asInt()));
+    UserDTO user = decodedToken(requestContext);
+    if (user != null) {
+      requestContext.setProperty("user", user);
     }
   }
 
