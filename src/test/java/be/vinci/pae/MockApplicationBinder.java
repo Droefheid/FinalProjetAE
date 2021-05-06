ddcea185b4573dba3bc32ae64bd688ee25522d6f
VAN MOER BRYAN <bryan.vanmoer@student.vinci.be>
2021-05-06 14:02:29 +0200

    prod2.properties update

--- src/test/java/be/vinci/pae/MockApplicationBinder.java
+++ src/test/java/be/vinci/pae/MockApplicationBinder.java
@@ -10,6 +10,8 @@ import be.vinci.pae.domaine.option.OptionUCC;
 import be.vinci.pae.domaine.option.OptionUCCImpl;
 import be.vinci.pae.domaine.user.UserUCC;
 import be.vinci.pae.domaine.user.UserUCCImpl;
+import be.vinci.pae.domaine.visit.VisitUCC;
+import be.vinci.pae.domaine.visit.VisitUCCImpl;
 import be.vinci.pae.services.DalBackendServices;
 import be.vinci.pae.services.DalServices;
 import be.vinci.pae.services.DalServicesImpl;
@@ -19,19 +21,21 @@ import be.vinci.pae.services.PhotoDAO;
 import be.vinci.pae.services.PhotoFurnitureDAO;
 import be.vinci.pae.services.TypeDAO;
 import be.vinci.pae.services.UserDAO;
+import be.vinci.pae.services.VisitDAO;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.ext.Provider;
 
 @Provider
 public class MockApplicationBinder extends AbstractBinder {
 
+  private DalServices dalServices = Mockito.mock(DalServices.class);
   private UserDAO userDAO = Mockito.mock(UserDAO.class);
   private FurnitureDAO furnitureDAO = Mockito.mock(FurnitureDAO.class);
   private TypeDAO typeDAO = Mockito.mock(TypeDAO.class);
   private OptionDAO optionDAO = Mockito.mock(OptionDAO.class);
-  private DalServices dalServices = Mockito.mock(DalServices.class);
   private PhotoDAO photoDAO = Mockito.mock(PhotoDAO.class);
   private PhotoFurnitureDAO photoFurnitureDAO = Mockito.mock(PhotoFurnitureDAO.class);
+  private VisitDAO visitDAO = Mockito.mock(VisitDAO.class);
 
 
 
@@ -44,10 +48,12 @@ public class MockApplicationBinder extends AbstractBinder {
     bind(optionDAO).to(OptionDAO.class);
     bind(photoDAO).to(PhotoDAO.class);
     bind(photoFurnitureDAO).to(PhotoFurnitureDAO.class);
+    bind(visitDAO).to(VisitDAO.class);
     bind(dalServices).to(DalServices.class);
     bind(UserUCCImpl.class).to(UserUCC.class).in(Singleton.class);
     bind(FurnitureUCCImpl.class).to(FurnitureUCC.class).in(Singleton.class);
     bind(OptionUCCImpl.class).to(OptionUCC.class).in(Singleton.class);
+    bind(VisitUCCImpl.class).to(VisitUCC.class).in(Singleton.class);
     bind(DalServicesImpl.class).to(DalBackendServices.class).in(Singleton.class);
   }
 }
--- src/test/java/be/vinci/pae/TestVisit.java
+++ src/test/java/be/vinci/pae/TestVisit.java
@@ -1,5 +1,90 @@
 package be.vinci.pae;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import be.vinci.pae.api.utils.BusinessException;
+import be.vinci.pae.domaine.DomaineFactory;
+import be.vinci.pae.domaine.address.AddressDTO;
+import be.vinci.pae.domaine.user.UserDTO;
+import be.vinci.pae.domaine.user.UserUCC;
+import be.vinci.pae.domaine.visit.VisitDTO;
+import be.vinci.pae.domaine.visit.VisitUCC;
+import be.vinci.pae.services.UserDAO;
+import be.vinci.pae.services.VisitDAO;
+
 public class TestVisit {
+
+
+  private DomaineFactory domaineFactory;
+
+  private VisitUCC visitUCC;
+  private VisitDTO visitDTO;
+  private VisitDAO visitDAO;
+
+  private UserUCC userUCC;
+  private UserDTO userDTO;
+  private UserDAO userDAO;
+
+  private AddressDTO addressDTO;
+
+  @BeforeEach
+  void initAll() {
+    ServiceLocator locator = ServiceLocatorUtilities.bind(new MockApplicationBinder());
+    this.domaineFactory = locator.getService(DomaineFactory.class);
+    this.userUCC = locator.getService(UserUCC.class);
+    this.userDAO = locator.getService(UserDAO.class);
+    this.visitUCC = locator.getService(VisitUCC.class);
+    this.visitDAO = locator.getService(VisitDAO.class);
+
+
+    userDTO = domaineFactory.getUserDTO();
+    addressDTO = domaineFactory.getAdressDTO();
+    visitDTO = domaineFactory.getVisitDTO();
+  }
+
+  /**
+   * Success Test :
+   */
+  @Test
+  public void testIntroduceOptionV1() {
+    Mockito.when(visitDAO.introduceVisit(visitDTO)).thenReturn(visitDTO);
+    assertEquals(visitDTO, visitUCC.introduceVisit(visitDTO, addressDTO, userDTO));
+  }
+
+  /**
+   * Fail Test :
+   */
+  @Test
+  public void testIntroduceOptionV2() {
+    Mockito.when(visitDAO.introduceVisit(visitDTO)).thenReturn(null);
+    assertThrows(BusinessException.class,
+        () -> visitUCC.introduceVisit(visitDTO, addressDTO, userDTO));
+  }
+
+
+  /**
+   * Success Test :
+   */
+  @Test
+  public void testGetVisitV1() {
+    Mockito.when(visitDAO.findById(visitDTO.getId())).thenReturn(visitDTO);
+    assertEquals(visitDTO, visitUCC.getVisit(visitDTO.getId()));
+  }
+
+  /**
+   * Fail Test :
+   */
+  @Test
+  public void testGetVisitV2() {
+    Mockito.when(visitDAO.findById(visitDTO.getId())).thenReturn(null);
+    assertThrows(BusinessException.class, () -> visitUCC.getVisit(visitDTO.getId()));
+  }
+
+
 
 }
