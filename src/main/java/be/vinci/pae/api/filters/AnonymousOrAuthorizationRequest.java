311b3f6f935be437481d5632ad329bb41481d847
Dorian Fouvez <dorian.fouvez@student.vinci.be>
2021-05-08 20:04:49 +0200

    refactor jenkins

--- src/main/java/be/vinci/pae/api/filters/AnonymousOrAuthorizationRequest.java
+++ src/main/java/be/vinci/pae/api/filters/AnonymousOrAuthorizationRequest.java
@@ -1,13 +1,6 @@
 package be.vinci.pae.api.filters;
 
 import java.io.IOException;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.JWTVerifier;
-import be.vinci.pae.domaine.DomaineFactory;
-import be.vinci.pae.domaine.user.UserUCC;
-import be.vinci.pae.utils.Config;
-import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
@@ -26,16 +19,6 @@ import jakarta.ws.rs.ext.Provider;
 @AnonymousOrAuthorize
 public class AnonymousOrAuthorizationRequest extends AuthorizeAbstract
     implements ContainerRequestFilter {
-
-  private final Algorithm jwtAlgorithm = Algorithm.HMAC256(Config.getProperty("JWTSecret"));
-  private final JWTVerifier jwtVerifier =
-      JWT.require(this.jwtAlgorithm).withIssuer("auth0").build();
-
-  @Inject
-  private UserUCC userUCC;
-
-  @Inject
-  private DomaineFactory domaineFactory;
 
   @Override
   public void filter(ContainerRequestContext requestContext) throws IOException {
