fac8bdc4d528dae35f648a2fa13248069d64137a
Dorian Fouvez <dorian.fouvez@student.vinci.be>
2021-05-10 21:30:34 +0200

    refactor jenkins

--- src/main/java/be/vinci/pae/api/VisitResource.java
+++ src/main/java/be/vinci/pae/api/VisitResource.java
@@ -97,13 +97,9 @@ public class VisitResource {
     visit.setLabelFurniture(json.get("label_furniture").asText());
 
     AddressDTO addressDTO = domaineFactory.getAdressDTO();
-
-    addressDTO.setBuildingNumber(json.get("building_number").asText());
-    addressDTO.setCommune(json.get("commune").asText());
-    addressDTO.setPostCode(json.get("postcode").asText());
-    addressDTO.setStreet(json.get("street").asText());
-    addressDTO.setUnitNumber(json.get("unit_number").asText());
-    addressDTO.setCountry(json.get("country").asText());
+    addressDTO = createFullFillAddress(addressDTO, -1, json.get("building_number").asText(),
+        json.get("commune").asText(), json.get("postcode").asText(), json.get("street").asText(),
+        json.get("unit_number").asText(), json.get("country").asText());
 
     if (currentUser.isBoss()) {
       int id = json.get("user_id").asInt();
@@ -290,6 +286,34 @@ public class VisitResource {
     if (!json.hasNonNull("unit_number") || json.get("unit_number").asText().equals("")) {
       throw new PresentationException("unit number is needed ", Status.BAD_REQUEST);
     }
+  }
+
+  /**
+   * create a full filled address.
+   * 
+   * @param addressDTO the address to fill.
+   * @param id of the address.
+   * @param buildingNumber of the address.
+   * @param commune of the address.
+   * @param postcode of the address.
+   * @param street of the address.
+   * @param unitNumber of the address.
+   * @param country of the address.
+   * @return the address full filled.
+   */
+  public static AddressDTO createFullFillAddress(AddressDTO addressDTO, int id,
+      String buildingNumber, String commune, String postcode, String street, String unitNumber,
+      String country) {
+
+    addressDTO.setID(id);
+    addressDTO.setBuildingNumber(buildingNumber);
+    addressDTO.setCommune(commune);
+    addressDTO.setPostCode(postcode);
+    addressDTO.setStreet(street);
+    addressDTO.setUnitNumber(unitNumber);
+    addressDTO.setCountry(country);
+
+    return addressDTO;
   }
 
 }
