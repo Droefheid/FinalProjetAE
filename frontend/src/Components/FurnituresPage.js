1a6d04f626e05a55add4b97fffc9eccf525cde71
Dorian Fouvez <dorian.fouvez@student.vinci.be>
2021-05-11 10:04:51 +0200

    remove unused searchBar

--- frontend/src/Components/FurnituresPage.js
+++ frontend/src/Components/FurnituresPage.js
@@ -53,13 +53,6 @@ const onFurnitureList = (types,data) => {
 
   if (!data) return;
   let table = `
-          <div class="input-group rounded" id="search_furniture_list">
-            <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
-              aria-describedby="search-addon" />
-            <span class="input-group-text border-0" id="search-addon">
-              <i class="fas fa-search"></i>
-            </span>
-          </div>
           <nav id="nav_furniture">
             <ul class="list-group">`;
   const user = getUserSessionData();
